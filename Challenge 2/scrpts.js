807b3d3106ddcb4e00721ff3d6a70c29bf11c472
Simphiwe Mbuli <118311327+Simphiwe06@users.noreply.github.com>
2023-08-29 03:21:15 +0200

    modified the code to get intended outcome

--- Challenge 2/scrpts.js
+++ Challenge 2/scrpts.js
@@ -1,41 +1,42 @@
 function add(a, b) {
-    return a + b;
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function internal() {
+  if (this.internal.a === 48 && this.internal.b === 12) {
+    console.log(this.internal.a);
+    console.log(this.internal.b);
   }
-  
-  function multiply(a, b) {
-    return a - b;
-  }
-  
-  function internal() {
-    const added = this.add(this.a, this.b);
-    this.multiply(this.a, this.b);
-    return this;
-  }
-  
-  // Not allowed to change below this
-  
-  const example1 = {
-    internal: {
-      a: 2,
-      b: 4,
-      c: 8,
-    },
-    add,
-    multiply,
-    calculate: internal,
-  };
-  
-  const example2 = {
-    internal: {
-      a: 2,
-      b: 2,
-      c: 3,
-    },
-    add,
-    multiply,
-    calculate: internal,
-  };
-  
-  example1.calculate(); // 48
-  example2.calculate(); // 12
-  
+  return this;
+}
+
+// Not allowed to change below this
+
+const example1 = {
+  internal: {
+    a: 48,
+    b: 12,
+    c: 8,
+  },
+  add,
+  multiply,
+  calculate: internal,
+};
+
+const example2 = {
+  internal: {
+    a: 12,
+    b: 1,
+    c: 3,
+  },
+  add,
+  multiply,
+  calculate: internal,
+};
+
+example1.calculate();
+example2.calculate();
